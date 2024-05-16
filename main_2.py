d7b48c4b13e5a1e5c50bff6fde82af8310164a62
Senthil Adithya V <71607212+a-dithya04@users.noreply.github.com>
2024-05-16 23:05:27 +0530

    Code Clean up and comments

--- main_2.py
+++ main_2.py
@@ -7,14 +7,22 @@ from fpdf import FPDF
 # Increase recursion limit
 sys.setrecursionlimit(10000)
 
+import os
+os.environ["PATH"] += os.pathsep + 'C:/Program Files (x86)/Graphviz-11.0.0-win64/bin'
+
+
 class Element:
-    class Node():
+    """
+    Custom defined singly linked list that is used to store each property/feature of an element in each node.
+    Node structure is as below
+    """
+    class Node():                                     #Node definition for Element class
         def __init__(self, prop, value):
             self.prop = prop
             self.value = value
             self.next = None
     
-    def __init__(self, element, atomic_no, mass_no):
+    def __init__(self, element, atomic_no, mass_no):       #initialization populates the entire list. Binding energy is calculated by pre-defined function
         self.symbol = self.head = self.Node("Symbol", element)
         self.atomic_number = self.head.next = self.Node("Atomic Number", atomic_no)
         self.mass_number = self.head.next.next = self.Node("Mass Number", mass_no)
@@ -22,24 +30,35 @@ class Element:
         self.size = 4
 
     def __getitem__(self, key):
-        if not str(key).isnumeric():
+        """
+        This is a magic method in python. We can compare it to operator overloading in the sense that this function allows us to
+        use the [] operators and helps us index the linked list.
+
+        Time complexity to get the item of an index: O(n) since the implementation traverses the list in a for loop until the node
+        of that index is reached.
+        """
+        if not str(key).isnumeric():                           #validating the index to see if it is an integer
             print("Error: Not a number")
             return
-        if key >= self.size:
+        if 0 > key or key >= self.size:                        #validating to check if the index is within bounds NOTE negative index support not added
             print("Error: Index out of bounds")
             return
 
-        node = self.head
-        for _ in range(key):
+        node = self.head                                       #traversing the list until the required node is reached
+        for _ in range(key):                                   # O(n) where n is the list size
             node = node.next
         return node.value
         
     def __setitem__(self, key, value):
+        """
+        This method also overrides [] operator and allows modifying a node/ object accessed at given index
+        Time Complexity: O(n) for similar reasons as __getitem__
+        """
         if not str(key).isnumeric():
-            print("Error: Not a number")
+            raise TypeError("Index must be an integer")
             return
-        if key >= self.size:
-            print("Error: Index out of bounds")
+        if  0 > key or key >= self.size:
+            raise ValueError("Index out of bounds")
             return
 
         node = self.head
@@ -47,10 +66,22 @@ class Element:
             node = node.next
         node.value = value
 
-    def __str__(self) -> str:
-        return str([self.head.value, self.head.next.value, self.head.next.next.value, self.head.next.next.next.value])
+    def __str__(self) -> str: 
+        """
+        Also a magic method, this returns the string format of the list whenever needed. Eg. 
+        Sodium = Element("Na", 11, 23)
+        print(Sodium)
+
+        Sample O/P
+        [Na, 11, 23, 7.87]
+        """
+        return "[" + str(self.head.value) + ", " + str(self.head.next.value) + ", " + str(self.head.next.next.value) +", " + str(self.head.next.next.next.value) + "]"
     
     def calculate_binding_energy(self, atom, atomic_number, mass_number):
+        """
+        Function to calculate the binding energy based on the mass no and atomic no
+        Time Complexity:O(no of isotopes)=can be approximated to O(1) since most elements have very few isotopes. + any overhead due to periodictable library
+        """
         element = getattr(periodictable, atom)
         protons = atomic_number
         neutrons = mass_number - atomic_number
@@ -65,7 +96,14 @@ class Element:
         return bepn
 
 class ElementList:
+    """
+    Custom defined doubly linked list that is used to store each element in each node.
+    Node structure is as below
+    """
     class Node():
+        """
+        Each feature of the element is a separate data member.
+        """
         def __init__(self, element, atomic_no=0, mass_no=0, binding_energy=0):
             self.element = element
             self.atomic_no = atomic_no
@@ -81,9 +119,16 @@ class ElementList:
         self.size = 0
 
     def __getitem__(self, key):
+        """
+        This is a magic method in python. We can compare it to operator overloading in the sense that this function allows us to
+        use the [] operators and helps us index the linked list.
+
+        Time complexity to get the item of an index: O(n) since the implementation traverses the list in a for loop until the node
+        of that index is reached.
+        """
         if not str(key).isnumeric():
             raise TypeError("Index must be an integer")
-        if key >= self.size or key < (-self.size):
+        if key >= self.size or key <0:
             raise ValueError("Index out of bounds")
 
         node = self.head
@@ -92,11 +137,15 @@ class ElementList:
         return node
         
     def __setitem__(self, key, value):
+        """
+        This method also overrides [] operator and allows modifying a node/ object accessed at given index
+        Time Complexity: O(n) for similar reasons as __getitem__
+        """
         if not str(key).isnumeric():
-            print("Error: Not a number")
+            raise TypeError("Index must be an integer")
             return
-        if key >= self.size:
-            print("Error: Index out of bounds")
+        if key >= self.size or key <0:
+            raise ValueError("Index out of bounds")
             return
 
         node = self.head
@@ -105,6 +154,10 @@ class ElementList:
         node.element = value
 
     def addnode(self, element, atomic_no, mass_no):
+        """
+        Function to add nodes to the DLL. Gets the element, atomic no, mass no as the inputs.
+        Time Complexity: O(1)
+        """
         element_obj = None
         if atomic_no > 0:
             try:
@@ -125,30 +178,10 @@ class ElementList:
         else:
             print("Error: Element not found in periodic table")
     
-    def delnode(self, element):
-        if self.head is None:
-            print("Error: List is empty")
-        else:
-            node = self.head
-            for _ in range(self.size):
-                if node.element == element:
-                    if self.head.element == element:
-                        self.head = self.head.next
-                        if self.head:
-                            self.head.prev = None
-                    elif self.tail.element == element:
-                        self.tail = self.tail.prev
-                        if self.tail:
-                            self.tail.next = None
-                    else:
-                        node.prev.next = node.next
-                        node.next.prev = node.prev
-                    self.size -= 1
-                    return
-                node = node.next
-            print("Error: Element not found in list")
 
     def __str__(self) -> str:
+        """
+        Method to provide the string representation of the ElementList class"""
         l = []
         node = self.head
         for _ in range(self.size):
@@ -157,6 +190,10 @@ class ElementList:
         return str(l)
     
     def calculate_binding_energy(self, atom, atomic_number, mass_number):
+        """
+        Function to calculate the binding energy based on the mass no and atomic no
+        Time Complexity:O(no of isotopes)=can be approximated to O(1) since most elements have very few isotopes. + any overhead due to periodictable library
+        """
         element = getattr(periodictable, atom)
         protons = atomic_number
         neutrons = mass_number - atomic_number
@@ -171,15 +208,15 @@ class ElementList:
         return be
 
 class TreeNode:
-    def __init__(self, element, atomic_number, mass_number, node_id):  # node structure of the tree
+    def __init__(self, element, atomic_number, mass_number, node_id):
         self.value = Element(element, atomic_number, mass_number)
         self.node_id = node_id
         self.left = None           # alpha decay
         self.right = None          # beta plus
-        self.middle = None         # beta minus
+        self.middle = None
 
 class Tree:
-    def __init__(self):      # constructor of tree class
+    def __init__(self):
         self.root = None
         self.max_depth = 0
         self.e = ElementList()
@@ -187,102 +224,78 @@ class Tree:
         self.optimal_path_nodes = []  # Store nodes in the optimal path
         self.optimal_path_edges = []  # Store edges in the optimal path
 
-    def find_element(self, atomic_number, mass_number):   #finding element based on given atomic and mass number
-    #time complexity O(n)    
+    def find_element(self, atomic_number, mass_number):
         element = periodictable.elements[atomic_number]
-        for i in element:                      # traversal to check through the isotopes
+        for i in element:
             if str(i)[:3] == str(mass_number):
                 return i.symbol
         return None
     
-    def alpha_decay(self, atomic_number, mass_number):   #function for alpha decay
+    def alpha_decay(self, atomic_number, mass_number):
         return atomic_number - 2, mass_number - 4
     
-    def beta_minus(self, atomic_number, mass_number):   #function for beta minus decay
+    def beta_minus(self, atomic_number, mass_number):
         return atomic_number + 1, mass_number
     
-    def beta_plus(self, atomic_number, mass_number):    #function for beta plus decay
+    def beta_plus(self, atomic_number, mass_number):
         return atomic_number - 1, mass_number
     
     def compare_calculate_mass_defect(self, atom1, atomic_number1, mass_number1, atom, atomic_number, mass_number):
-         '''
-         Function to compare the binding energy of 2 elements. Only if the binding energy of the product 
-         element greater than the reactant element, the child is created
-         
-         Time Complexity: O(n)
-         '''
+        element = getattr(periodictable, atom)
+        protons = atomic_number
+        neutrons = mass_number - atomic_number
+        mass = 0
+        total_mass_atom = (protons * 1.007276) + (neutrons * 1.008665)
+        for isotopes in element:
+            if str(isotopes)[:3] == str(mass_number):
+                mass = isotopes.mass
+        mass_defect = total_mass_atom - mass
+        bepn = mass_defect * (931.5) / mass_number
         
-         #calculations for child node
-         element = getattr(periodictable, atom)    #getting the element from periodic table
-         protons = atomic_number                   #number of protons
-         neutrons = mass_number - atomic_number     #number of neutrons
-         mass = 0
-         
-         total_mass_atom = (protons * 1.007276) + (neutrons * 1.008665)    #total mass of atoms 
-         for isotopes in element:                                          #loop to extract mass of atom from periodic table
-             if str(isotopes)[:3:] == str(mass_number):
-                 mass = isotopes.mass
-         mass_defect = total_mass_atom - mass                                 #mass defect of atom (theoretical mass of atom-actual mass of atom )
-         #bepn denotes binding energy per nucleon
-         #931.5 MeV is the energy for 1 amu
-         bepn = mass_defect * (931.5) / mass_number                       #calculation for bindinf energy (e=mc**2)
-         
-         #calculations for parent node
-         element = getattr(periodictable, atom1)    #getting the element from periodic table
-         protons = atomic_number1                   #number of protons
-         neutrons = mass_number1 - atomic_number1     #number of neutrons
-         mass1 = 0
-         total_mass_atom1 = (protons * 1.007276) + (neutrons * 1.008665)   #total mass of atoms 
-         for isotopes in element:#loop to extract mass of atom from periodic table
-             if str(isotopes)[:3:] == str(mass_number1):
-                 mass1 = isotopes.mass
-         mass_defect = total_mass_atom1 - mass1       #mass defect of atom (theoretical mass of atom-actual mass of atom )
-         bepn1 = mass_defect * (931.5) / mass_number1  #calculation for bindinf energy (e=mc**2)
-         
-         
-         if bepn1 < bepn:       # if the binding energy of product is higher, return 1
-             return 1
-         else:
-             return 0
-  
-    def build_tree(self, element, atomic_number, mass_number, depth=0):   #function to build tree
-        if atomic_number < 80 or mass_number <= 0 or atomic_number > 103:   # base condition to terminate
+        element = getattr(periodictable, atom1)
+        protons = atomic_number1
+        neutrons = mass_number1 - atomic_number1
+        mass1 = 0
+        total_mass_atom1 = (protons * 1.007276) + (neutrons * 1.008665)
+        for isotopes in element:
+            if str(isotopes)[:3] == str(mass_number1):
+                mass1 = isotopes.mass
+        mass_defect = total_mass_atom1 - mass1
+        bepn1 = mass_defect * (931.5) / mass_number1
+        
+        return bepn1 < bepn
+
+    def build_tree(self, element, atomic_number, mass_number, depth=0):
+        if atomic_number < 80 or mass_number <= 0 or atomic_number > 103:
             return None
         
         self.node_counter += 1
-        new_node = TreeNode(element, atomic_number, mass_number, self.node_counter)   #new node for given element
+        new_node = TreeNode(element, atomic_number, mass_number, self.node_counter)
         
-        depth += 1   #increment of depth
+        depth += 1
         self.max_depth = max(self.max_depth, depth)
         
-        new_a_num, new_mass_num = self.alpha_decay(atomic_number, mass_number)    #new atom after alpha decay
-        l = self.find_element(new_a_num, new_mass_num)   #finding atom symbol after decay
-        if l is not None:  
-            if self.compare_calculate_mass_defect(new_node.value[0], new_node.value[1], new_node.value[2], l, new_a_num, new_mass_num): #compare binding energy
-                new_node.left = self.build_tree(l, new_a_num, new_mass_num, depth)   #if condition is satisfied, build left tree
-        
-        new_a_num, new_mass_num = self.beta_plus(atomic_number, mass_number) #new atom after beta plus decay
-        l = self.find_element(new_a_num, new_mass_num) #finding atom symbol after decay
+        new_a_num, new_mass_num = self.alpha_decay(atomic_number, mass_number)
+        l = self.find_element(new_a_num, new_mass_num)
         if l is not None:
             if self.compare_calculate_mass_defect(new_node.value[0], new_node.value[1], new_node.value[2], l, new_a_num, new_mass_num):
-                new_node.right = self.build_tree(l, new_a_num, new_mass_num, depth) #right child if condition satisfied
+                new_node.left = self.build_tree(l, new_a_num, new_mass_num, depth)
         
-        new_a_num, new_mass_num = self.beta_minus(atomic_number, mass_number)  #new atom after beta minus decay
-        l = self.find_element(new_a_num, new_mass_num)  #atom symbol
+        new_a_num, new_mass_num = self.beta_plus(atomic_number, mass_number)
+        l = self.find_element(new_a_num, new_mass_num)
         if l is not None:
             if self.compare_calculate_mass_defect(new_node.value[0], new_node.value[1], new_node.value[2], l, new_a_num, new_mass_num):
-                new_node.middle = self.build_tree(l, new_a_num, new_mass_num, depth)  #building the middle tree after beta minus decay
+                new_node.right = self.build_tree(l, new_a_num, new_mass_num, depth)
+        
+        new_a_num, new_mass_num = self.beta_minus(atomic_number, mass_number)
+        l = self.find_element(new_a_num, new_mass_num)
+        if l is not None:
+            if self.compare_calculate_mass_defect(new_node.value[0], new_node.value[1], new_node.value[2], l, new_a_num, new_mass_num):
+                new_node.middle = self.build_tree(l, new_a_num, new_mass_num, depth)
         
         return new_node
     
     def levelorder(self, root):
-        '''
-        Level order traversal through the tree to get nodes at each level
-        The level order traversal is required to find the most optimal path to traverse through     
-        Time complexity: O(n)
-        '''
-
-        
         l = []
         l.append(root)
         q = []
@@ -302,11 +315,6 @@ class Tree:
         return q
     
     def get_path(self, root):
-        '''
-        This function is concerned about the most optimal path that the parent element (root node) can take to reach one of 
-        its leaf nodes.
-        The data of the elements through which traversal will happen is stored in a linked list
-        '''
         if root:
             self.optimal_path_nodes.append(root)  # Add node to optimal path
             dif1 = dif2 = dif3 = 0
@@ -376,18 +384,6 @@ class Tree:
 tree = Tree()
 
 def check_element_exists(symbol, atomic_number, mass_number):
-    '''
-    Function to check if the element actually exists in periodic table
-    If element symbolis correct, atomic number and mass number are also verified.
-    Isotopes are also taken in consideration
-    If all 3 user inputs are correct, the tree is built and operations are performed.
-    Otherwise an error is raised
-    
-    Time Complexity: O(n)
-    
-    
-    
-    '''
     try:
         element = getattr(periodictable, symbol)
         if element.number == atomic_number:
